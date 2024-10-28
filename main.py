e88a16931c695244aafb7bd8f5f5ecb88a9d4ead
minhtetza <128377466+minhtetza@users.noreply.github.com>
2024-10-29 03:41:08 +0630

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ from multiprocessing import Process
 import threading
 from bs4 import BeautifulSoup
 stopuser = {}
-token = '7519786086:AAFt0dqsL_pWIemheTxZNeQkorUKXxo3Zzo'
+token = '7654644825:AAEU9XqRUGwGc7l7p-TrJ7jceSJzdFr3l9M'
 bot=telebot.TeleBot(token,parse_mode="HTML")
 admin=5688879944 
 command_usage = {}
