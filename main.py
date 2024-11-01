d3d1ba7fc8b12238a111f06ec2ec3342bcb7d965
minhtetza <128377466+minhtetza@users.noreply.github.com>
2024-11-01 10:26:20 +0630

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ import time
 from telebot import types
 
 stopuser = {}
-token = "7336963132:AAF8WSITFgn_FULM1kqdTrC0tnqeKZmrzsY"
+token = "7336963132:AAHGi-AKzKTkXJieOb1gExTodSjTDXStKFA"
 bot=telebot.TeleBot(token,parse_mode="HTML")
 
 
